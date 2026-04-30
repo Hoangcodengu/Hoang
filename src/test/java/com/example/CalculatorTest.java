08422cbcf2dddac0b80be6da753448df88b11de4
25021776-hash <25021776@vnu.edu.vn>
2026-04-30 16:25:14 +0700

    Bai 5

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -8,7 +8,7 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(10, calculator.add(2, 3));
+        assertEquals(5, calculator.add(2, 3));
     }
 
     @Test
